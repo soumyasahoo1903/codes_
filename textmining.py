11c73cb2f066c3a60352a17bdc4e5775ec2bd9e4
soumya sahoo <135194013+soumyasahoo1903@users.noreply.github.com>
2023-06-26 10:21:08 +0530

    Update textmining.py

--- textmining.py
+++ textmining.py
@@ -1,133 +1,109 @@
+from Bio import Entrez, Medline
 import pandas as pd
-from bs4 import BeautifulSoup
 import requests
-from Bio import Entrez
-import re
+from bs4 import BeautifulSoup
+import time
 
-# Set up NLTK and download necessary resources
-import nltk
-nltk.download('punkt')
+def search_pubchem(metabolite):
+    url = f'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{metabolite}/cids/JSON'
+    response = requests.get(url)
+    data = response.json()
+    if 'IdentifierList' in data and 'CID' in data['IdentifierList']:
+        return data['IdentifierList']['CID']
+    return []
 
-# Read the Excel file
-excel_file = 'F:/NISER internship/Text-minning/metabolite.xlsx'
-df = pd.read_excel(excel_file)
+def search_metabolite_functions(metabolites):
+    Entrez.email = 'your.email@example.com'
+    results = []
+    for metabolite in metabolites:
+        # Search for metabolite functions in PubChem
+        pubchem_results = search_pubchem(metabolite)
+        if pubchem_results:
+            results.append(pubchem_results)
+        else:
+            # If not found in PubChem, search in PubMed for freely available full-text papers
+            term = f'{metabolite} AND "free full text"[Filter]'
+            handle = Entrez.esearch(db='pubmed', sort='relevance', retmax='40', term=term)
+            result = Entrez.read(handle)
+            results.append(result["IdList"])
+    return results
 
-# Extract the metabolite names from the 'Metabolite name' column
-metabolites = df['Metabolites'].tolist()
+def search_google_scholar(metabolite):
+    query = f'{metabolite} metabolite function'
+    url = f'https://scholar.google.com/scholar?q={query}'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    result_links = soup.find_all('h3', class_='gs_rt')
+    results = []
+    for link in result_links:
+        article_title = link.text.strip()
+        article_url = link.a['href']
+        article_id = extract_article_id(article_url)
+        results.append(article_id)
+    return results
 
-# Lists to store functions, links, and hits
-functions = []
-links = []
-hits = []
+def extract_article_id(url):
+    # Extract article ID from the URL
+    article_id = url.split('scholar?')[1]
+    return article_id
 
-# Keywords related to the gut microbiome or gut microorganisms
-gut_keywords = ['barrier integrity', 'epithelial integrity', 'intestinal stem cells', 'epithelial cell proliferation', 'wnt signalling pathway', 'cell differentiation', 'intestinal epithelial cells']
+def fetch_metabolite_details(id_lists):
+    Entrez.email = 'your.email@example.com'
+    results = []
+    for id_list in id_lists:
+        ids = ','.join(id_list)
+        handle = Entrez.efetch(db='pubmed', rettype='medline', retmode='text', id=ids)
+        records = Medline.parse(handle)
+        records = list(records)
+        functions = []
+        for record in records:
+            functions.append(record.get('AB', 'N/A'))
+        results.append(functions)
+    return results
 
-# Initialize sheet for metabolites without gut keyword mentions
-no_keyword_df = pd.DataFrame(columns=['Metabolites', 'Function'])
+def process_metabolites(excel_file, output_file_prefix, batch_size=5):
+    # Read the Excel file
+    df = pd.read_excel(excel_file)
 
-# Iterate through each metabolite
-for metabolite in metabolites:
-    function = ''
-    link = ''
-    hit_count = 0
+    # Extract the metabolite names from the 'Metabolite' column
+    metabolites = df['Metabolite'].tolist()
 
-    # Search Metabolomics Workbench for metabolite functions
-    mw_keywords = metabolite + ' ' + ' OR '.join(gut_keywords)
-    mw_search_url = f"https://www.metabolomicsworkbench.org/search/?query={mw_keywords}"
-    mw_response = requests.get(mw_search_url)
-    if mw_response.status_code == 200:
-        soup = BeautifulSoup(mw_response.content, "html.parser")
-        search_results = soup.find_all("h4", class_="card-title")
-        for i, result in enumerate(search_results[:3]):
-            title = result.text.strip()
-            function += f"Metabolomics Workbench Result {i+1}: {title}\n"
-            link += f"Metabolomics Workbench Result {i+1}: [Insert link to the article here]\n"
-            hit_count += 1
+    total_metabolites = len(metabolites)
+    total_batches = (total_metabolites + batch_size - 1) // batch_size  # Round up division
 
-    # Search KEGG database for metabolite functions
-    kegg_keywords = metabolite + ' ' + ' OR '.join(gut_keywords)
-    kegg_search_url = f"https://www.kegg.jp/dbget-bin/www_bget?-f+m+compound+{kegg_keywords}"
-    kegg_response = requests.get(kegg_search_url)
-    if kegg_response.status_code == 200:
-        soup = BeautifulSoup(kegg_response.content, "html.parser")
-        kegg_result = soup.find("pre")
-        if kegg_result:
-            function += f"KEGG Result:\n{kegg_result.text.strip()}\n"
-            link += f"KEGG Result: [Insert link to the article here]\n"
-            hit_count += 1
+    for batch in range(total_batches):
+        start_idx = batch * batch_size
+        end_idx = (batch + 1) * batch_size
+        batch_metabolites = metabolites[start_idx:end_idx]
 
-    # Query PubChem API for metabolite functions
-    pubchem_response = requests.get(f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{metabolite}/description/JSON")
-    if pubchem_response.status_code == 200:
-        pubchem_data = pubchem_response.json()
-        if 'InformationList' in pubchem_data:
-            pubchem_information = pubchem_data['InformationList']['Information']
-            for info in pubchem_information:
-                if 'Description' in info:
-                    function += f"PubChem Result: {info['Description']}\n"
-                if 'CID' in info:
-                    link += f"PubChem Result: https://pubchem.ncbi.nlm.nih.gov/compound/{info['CID']}\n"
-            hit_count += 1
+        # Search for metabolite functions
+        id_lists = search_metabolite_functions(batch_metabolites)
 
-    # Query PubMed for metabolite functions
-    Entrez.email = "priyanshupandabcd@gmail.com"  # Replace with your email address
-    pubmed_query = metabolite + ' ' + ' OR '.join(gut_keywords)
-    handle = Entrez.esearch(db="pubmed", term=pubmed_query, retmax=3)
-    pubmed_records = Entrez.read(handle)
-    handle.close()
-    pubmed_ids = pubmed_records["IdList"]
+        # Fetch details of the metabolite functions
+        results = fetch_metabolite_details(id_lists)
 
-    for pubmed_id in pubmed_ids:
-        pubmed_summary = None
-        pubmed_link = f"https://pubmed.ncbi.nlm.nih.gov/{pubmed_id}"
-        try:
-            handle = Entrez.efetch(db="pubmed", id=pubmed_id, rettype="xml", retmode="text")
-            record = Entrez.read(handle)
-            pubmed_summary = record["PubmedArticle"][0]["MedlineCitation"]["Article"]["Abstract"]["AbstractText"][0]
-            handle.close()
-        except:
-            pass
+        # Flatten the nested list of functions
+        flattened_results = [function for sublist in results for function in sublist]
 
-        if pubmed_summary:
-            if any(keyword.lower() in pubmed_summary.lower() for keyword in gut_keywords):
-                function += f"PubMed Result: {pubmed_summary}\n"
-                link += f"PubMed Result: {pubmed_link}\n"
-                hit_count += 1
+        # Update the 'Function' column with the function results
+        functions_dict = {metabolite: function for metabolite, function in zip(batch_metabolites, flattened_results)}
+        df.loc[start_idx:end_idx-1, 'Function'] = df.loc[start_idx:end_idx-1, 'Metabolite'].map(functions_dict).fillna('N/A')
 
-    # Search Google Scholar for metabolite functions related to the gut microbiome
-    google_keywords = f"{metabolite} {' OR '.join(gut_keywords)}"
-    google_search_url = f"https://scholar.google.com/scholar?q={google_keywords}"
-    google_response = requests.get(google_search_url)
-    if google_response.status_code == 200:
-        soup = BeautifulSoup(google_response.content, "html.parser")
-        search_results = soup.find_all("div", class_="gs_ri")
-        for i, result in enumerate(search_results[:3]):
-            title_element = result.find("h3", class_="gs_rt")
-            abstract_element = result.find("div", class_="gs_rs")
-            if title_element and abstract_element:
-                title = title_element.text.strip()
-                abstract = abstract_element.text.strip()
-                if any(keyword.lower() in abstract.lower() for keyword in gut_keywords):
-                    function += f"Google Scholar Result: {title}\n"
-                    link += f"Google Scholar Result: [Insert link to the article here]\n"
-                    hit_count += 1
+        # Save the updated DataFrame to a new Excel file
+        output_file = f"{output_file_prefix}_{batch + 1}.xlsx"
+        df.loc[start_idx:end_idx-1].to_excel(output_file, index=False)
+        
+        # Wait for 1 second before processing the next batch
+        time.sleep(1)
 
-    # Add metabolite, function, and hit count to respective lists
-    functions.append(function)
-    links.append(link)
-    hits.append(hit_count)
+def main():
+    # Input file and output file prefix
+    excel_file = "F:/NISER internship/Text-minning/unique_metabolites.xlsx"
+    output_file_prefix = "F:/NISER internship/Text-minning/m"
 
-    # If no gut keywords found, move to a separate sheet
-    if hit_count == 0:
-        no_keyword_df = no_keyword_df.append({'Metabolites': metabolite, 'Function': function}, ignore_index=True)
+    # Process metabolites in batches
+    process_metabolites(excel_file, output_file_prefix, batch_size=100)
 
-# Add new columns for function, links, and hits to the DataFrame
-df['Function'] = functions
-df['Links'] = links
-df['Hits'] = hits
 
-# Write the updated DataFrame back to the Excel file
-with pd.ExcelWriter(excel_file) as writer:
-    df.to_excel(writer, index=False, sheet_name='Main Sheet')
-    no_keyword_df.to_excel(writer, index=False, sheet_name='No Keyword Mentions')
+if __name__ == '__main__':
+    main()
