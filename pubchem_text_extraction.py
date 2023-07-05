707ba7dcd040c7036bbf1b9bc031c7dfba7232fd
soumya sahoo <135194013+soumyasahoo1903@users.noreply.github.com>
2023-07-05 23:33:08 +0530

    Update pubchem_text_extraction.py

--- pubchem_text_extraction.py
+++ pubchem_text_extraction.py
@@ -2,28 +2,33 @@ import requests
 import xml.etree.ElementTree as ET
 import pandas as pd
 
-# Function to fetch the extracted text for a compound ID
-def fetch_extracted_text(compound_id):
-    # Base URL for PubChem compound data
-    base_url = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/{}/XML/?response_type=display'
+# Function to fetch the extracted text for a list of compound IDs
+def fetch_extracted_texts(compound_ids):
+    extracted_texts = []
+    # Iterate over the compound IDs
+    for compound_id in compound_ids:
+        # Base URL for PubChem compound data
+        base_url = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/{}/XML/?response_type=display'
 
-    # Format the URL with the compound ID
-    url = base_url.format(compound_id)
+        # Format the URL with the compound ID
+        url = base_url.format(compound_id)
 
-    # Fetch the XML content
-    response = requests.get(url)
-    xml_content = response.content.decode('utf-8')
+        # Fetch the XML content
+        response = requests.get(url)
+        xml_content = response.content.decode('latin-1')  # Decode using 'latin-1'
 
-    # Parse the XML content
-    root = ET.fromstring(xml_content)
+        # Parse the XML content
+        root = ET.fromstring(xml_content)
 
-    # Find the first five String elements
-    string_elements = root.findall('.//{http://pubchem.ncbi.nlm.nih.gov/pug_view}String')[:15]
-    extracted_text = [string_element.text.strip() for string_element in string_elements]
-    return extracted_text 
+        # Find the first five String elements
+        string_elements = root.findall('.//{http://pubchem.ncbi.nlm.nih.gov/pug_view}String')[:100]
+        extracted_text = [string_element.text.strip() for string_element in string_elements]
+        extracted_texts.append(extracted_text)
+    
+    return extracted_texts
 
 # Read the Excel file
-df = pd.read_excel(r'F:\NISER internship\Text-minning\pubchem_id_description\7179_metabolites\example.xlsx')
+df = pd.read_excel(r'F:\NISER internship\Text-minning\pubchem_id_description\7179_metabolites\New folder\Metabolites_with_pubchem_ID(5068).xlsx')
 
 # Get the compound IDs from the second column
 compound_ids = df.iloc[:, 1].tolist()
@@ -31,13 +36,23 @@ compound_ids = df.iloc[:, 1].tolist()
 # Create a new column for extracted text
 df['Extracted Text'] = ''
 
-# Iterate over the compound IDs
-for i, compound_id in enumerate(compound_ids, start=0):
-    # Fetch the extracted text
-    extracted_text = fetch_extracted_text(compound_id)
-    
-    # Store the extracted text in the corresponding row of the new column
-    df.at[i, 'Extracted Text'] = extracted_text
+# Set the batch size
+batch_size = 100
 
-# Save the modified DataFrame back to the Excel file
-df.to_excel(r'F:\NISER internship\Text-minning\pubchem_id_description\7179_metabolites\example.xlsx', index=False)
+# Process compound IDs in batches
+for i in range(0, len(compound_ids), batch_size):
+    # Get the current batch of compound IDs
+    batch_compound_ids = compound_ids[i:i+batch_size]
+
+    # Fetch the extracted texts for the current batch
+    extracted_texts = fetch_extracted_texts(batch_compound_ids)
+
+    # Update the extracted texts in the corresponding rows of the DataFrame
+    for j, extracted_text in enumerate(extracted_texts):
+        df.at[i+j, 'Extracted Text'] = extracted_text
+
+    # Save the modified DataFrame to a new Excel file after processing each batch
+    df.to_excel(r'F:\NISER internship\Text-minning\pubchem_id_description\7179_metabolites\New folder\Metabolites_with_pubchem_ID_processed.xlsx', index=False)
+
+# Save the final modified DataFrame back to the original Excel file
+df.to_excel(r'F:\NISER internship\Text-minning\pubchem_id_description\7179_metabolites\New folder\Metabolites_with_pubchem_ID_processed.xlsx', index=False)
